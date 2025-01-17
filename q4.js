a81d8ff943c32286c1506206c3b43c830d5a4731
EmilyM <155667566+EmilyMenken@user.noreply.github.com>
2025-01-17 11:53:34 -0800

    Finished Q3 and Q4

--- q4.js
+++ q4.js
@@ -3,12 +3,25 @@ A palindrome is a word, number, or other sequence of characters that reads the s
 */
 
 function checkPalindrome(textInput) {
-    
+
+    for (let i = 0; i < textInput.length / 2; i++) {
+        if (textInput[i] !== textInput[textInput.length - 1 - i]) //compares the letters in the string
+            {
+            return false; 
+        }
+    }
+    return true; 
 }
 
-// Test for palindrome
-
+// Testing
 let testString = "racecar"; // true
 let testString2 = "hello"; // false
+//extra test to make sure it don't just look at the first and last letter
+let testString3 = "ceramic"; //false
 
-// TODO: display the result of the test strings in the console
+console.log(testString +": ");
+console.log(checkPalindrome(testString)); // Expected res: true
+console.log(testString2 +": ");
+console.log(checkPalindrome(testString2)); // Expected res: false
+console.log(testString3 +": ");
+console.log(checkPalindrome(testString3)); //expected res: false
